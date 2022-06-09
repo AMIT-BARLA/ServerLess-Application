c00cb461e5de6b381c67a4fae0f2228c3775adf3
AMIT BARLA <amitbarlait@gmail.com>
2022-06-09 16:12:40 +0530

    Serverless Application- AWS Workshop

---
+++ wildrydes-site/js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'ap-south-1_S6g6AzNss', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '78p13sauk51apv2ljbo6p6uact', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-south-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://hx9n37joz9.execute-api.ap-south-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
